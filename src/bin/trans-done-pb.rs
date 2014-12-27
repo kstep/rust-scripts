938370910c2b79aea7fbaa425d01774845c8cae0
Konstantin Stepanov <me@kstep.me>
2014-12-27 08:43:04 +0300

    read creds from config

--- src/bin/trans-done-pb.rs
+++ src/bin/trans-done-pb.rs
@@ -1,14 +1,32 @@
 #![feature(slicing_syntax)]
 
 extern crate pb;
+extern crate xdg;
+extern crate toml;
+extern crate "rustc-serialize" as serialize;
 
 use pb::api::PbAPI;
 use pb::messages::{PushMsg, TargetIden};
 use pb::objects::{Push, PushData};
 use std::os::getenv;
+use std::io::File;
+use xdg::XdgDirs;
+use serialize::Decodable;
+
+#[deriving(Decodable)]
+struct Config {
+    access_token: String
+}
+
+fn load_config() -> Option<Config> {
+    XdgDirs::new().want_read_config("pushbullet/creds.toml")
+        .and_then(|ref p| File::open(p).ok())
+        .and_then(|mut f| f.read_to_string().ok())
+        .and_then(|s| toml::decode_str(s[]))
+}
 
 fn main() {
-    let api = PbAPI::new(getenv("PB_API_KEY").unwrap()[]);
+    let api = PbAPI::new(load_config().unwrap().access_token[]);
     let torrent_name = getenv("TR_TORRENT_NAME").unwrap();
     let torrent_dir = getenv("TR_TORRENT_DIR").unwrap();
     let push = PushMsg {

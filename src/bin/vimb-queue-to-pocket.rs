1ac414c9fad9d96ab4142b4d7e5f42ff1e6fc5da
Konstantin Stepanov <me@kstep.me>
2015-01-20 18:33:40 +0300

    implement vimb queue to pocket daemon

--- src/bin/vimb-queue-to-pocket.rs
+++ src/bin/vimb-queue-to-pocket.rs
@@ -1,17 +1,42 @@
 #![allow(unstable)]
 
+extern crate pocket;
 extern crate inotify;
+extern crate xdg;
+extern crate "script-utils" as utils;
+extern crate "rustc-serialize" as rustc_serialize;
 
+use pocket::Pocket;
 use inotify::{INotify, ffi};
-use std::path::Path;
+use std::io::fs::File;
+use std::io::BufferedReader;
+use xdg::XdgDirs;
+
+#[derive(RustcDecodable)]
+struct Creds {
+    consumer_key: String,
+    access_token: String,
+}
 
 fn main() {
+    let config: Creds = utils::load_config("pocket/creds.toml").expect("config file load error");
+    let mut pocket = Pocket::new(&*config.consumer_key, Some(&*config.access_token));
+
+    let xdgdirs = XdgDirs::new();
+
+    let queue = xdgdirs.want_read_config("vimb/queue").expect("no vimb config found");
+
     let mut inotify = INotify::init().unwrap();
-    let watcher = inotify.add_watch(&Path::new("/home/kstep/.config/vimb/queue"), ffi::IN_CLOSE_WRITE).unwrap();
+    inotify.add_watch(&queue, ffi::IN_CLOSE_WRITE).unwrap();
 
     loop {
-        for ev in inotify.wait_for_events().unwrap().iter() {
-            println!("{:?}", ev);
+        inotify.wait_for_events().unwrap();
+        let mut reader = BufferedReader::new(File::open(&queue).unwrap());
+        for line in reader.lines() {
+            match pocket.add(&*line.unwrap().trim()) {
+                Ok(item) => println!("added url {}", item.given_url),
+                Err(err) => println!("error: {:?}", err)
+            }
         }
     }
 }

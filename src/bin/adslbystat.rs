4707a4948c2cbab53077647d17232d0ae1a2336e
Konstantin Stepanov <me@kstep.me>
2015-12-10 00:33:59 +0300

    adslbystat: better error reporting and credit activation support

--- src/bin/adslbystat.rs
+++ src/bin/adslbystat.rs
@@ -13,10 +13,12 @@ extern crate regex;
 extern crate script_utils as utils;
 
 use hyper::client::Client;
-use hyper::header::{Authorization, Basic};
+use hyper::header::{Authorization, Referer, Basic};
+use hyper::error::Result;
 use encoding::{Encoding, DecoderTrap};
 use encoding::all::WINDOWS_1251;
 use std::process::exit;
+use std::env;
 use std::fmt;
 use std::io::{Read};
 
@@ -45,11 +47,28 @@ impl fmt::Display for AcctInfo {
     }
 }
 
-#[derive(RustcDecodable, Debug)]
+#[derive(RustcDecodable, Debug, Clone)]
 struct Creds {
     username: String,
     password: String
 }
+
+fn enable_credit(creds: Creds) -> Result<bool> {
+    let client = Client::new();
+    client.get("https://www.adsl.by/credit.js?credit=on")
+        .header(Authorization(Basic { username: creds.username, password: Some(creds.password) }))
+        .header(Referer("https://www.adsl.by".into()))
+        .send()
+        .and_then(|mut resp| {
+            let mut buf = String::new();
+            resp.read_to_string(&mut buf).map_err(::hyper::error::Error::Io)
+                .map(|_| buf.contains("stat: 'Включен'"))
+        })
+}
+
+const EXIT_ENABLED: i32 = 0;
+const EXIT_DISABLED: i32 = 1;
+const EXIT_ERROR: i32 = 2;
 
 fn main() {
     let state_re = regex!(r">Аккаунт</td>\s*<td class='right'><b>Включен<");
@@ -58,20 +77,38 @@ fn main() {
     let price_re = regex!(r"тариф</td>\s*<td class='right'><b>(\d+) ");
     let credit_re = regex!(r"кредит</td>\s*<td class='right'><b>(\d+)%");
 
-    let config: Creds = utils::load_config("adslby/creds.toml").expect("config file load error");
+    let config: Creds = match utils::load_config("adslby/creds.toml") {
+        Some(conf) => conf,
+        None => {
+            println!("Config file load error.");
+            exit(EXIT_ERROR);
+        }
+    };
 
     let client = Client::new();
     //client.set_ssl_verifier(Box::new(utils::permissive_ssl_checker));
 
-    let mut buf = Vec::new();
-    client.get("https://www.adsl.by/001.htm")
-        .header(Authorization(Basic { username: config.username, password: Some(config.password) }))
+    let buf = match client.get("https://www.adsl.by/001.htm")
+        .header(Authorization(Basic { username: config.username.clone(), password: Some(config.password.clone()) }))
         .send()
-        .unwrap()
-        .read_to_end(&mut buf)
-        .unwrap();
+        .and_then(|mut resp| {
+            let mut buf = Vec::new();
+            resp.read_to_end(&mut buf).map(|_| buf).map_err(::hyper::error::Error::Io)
+        }) {
+            Ok(buf) => buf,
+            Err(err) => {
+                println!("Error requesting account stats: {}", err);
+                exit(EXIT_ERROR);
+            }
+        };
 
-    let cont = WINDOWS_1251.decode(&*buf, DecoderTrap::Replace).unwrap();
+    let cont = match WINDOWS_1251.decode(&*buf, DecoderTrap::Replace) {
+        Ok(res) => res,
+        Err(err) => {
+            println!("Error decoding HTML page: {}", err);
+            exit(EXIT_ERROR);
+        }
+    };
 
     let acct = AcctInfo {
             enabled: state_re.is_match(&*cont),
@@ -82,7 +119,20 @@ fn main() {
         };
 
     println!("{}", acct);
-    exit(if acct.enabled { 0 } else { 1 });
+
+    exit(if acct.enabled {
+        EXIT_ENABLED
+    } else {
+        if env::args().position(|arg| arg == "credit").is_some() {
+            match enable_credit(config) {
+                Ok(true) => { println!("Credit was enabled."); EXIT_ENABLED },
+                Ok(false) => { println!("Credit was not enabled."); EXIT_DISABLED },
+                Err(err) => { println!("Error enabling credit: {}", err); EXIT_ERROR },
+            }
+        } else {
+            EXIT_DISABLED
+        }
+    });
 }
 
 #[bench]

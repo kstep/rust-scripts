04b36aa0b36c73a69c111fad510fef212567b5be
Konstantin Stepanov <me@kstep.me>
2015-12-13 03:07:01 +0300

    use walkdir crate

--- src/bin/nginx-cache-inspector.rs
+++ src/bin/nginx-cache-inspector.rs
@@ -1,14 +1,14 @@
-#![feature(fs_walk)]
-
 extern crate url;
 extern crate serde;
+extern crate walkdir;
 
 use std::path::Path;
-use std::fs::{File, walk_dir};
+use std::fs::File;
 use std::io::{BufReader, BufRead, Read};
 use std::env;
 use std::mem::transmute;
 use url::Url;
+use walkdir::WalkDir;
 
 #[allow(dead_code)]
 struct NginxCacheHeader {
@@ -28,9 +28,11 @@ impl NginxCacheHeader {
 fn main() {
     let args : Vec<String> = env::args().collect();
     let root = Path::new(if args.len() < 2 { "/var/lib/nginx/cache" } else { &*args[1] });
-    let files = walk_dir(&root).unwrap_or_else(|e| panic!("Nginx cache dir access error: {}", e))
+    let files = WalkDir::new(&root)
+        .into_iter()
         .filter_map(|e| e.ok())
-        .map(|e| e.path()).filter(|p| p.is_file())
+        .filter(|e| e.file_type().is_file())
+        .map(|e| e.path().to_owned())
         .filter_map(|p| File::open(&p).ok().map(|f| BufReader::new(f))
                     .and_then(|mut f| {
                         let mut buf = [0u8; HEADER_SIZE];
@@ -38,8 +40,7 @@ fn main() {
                         let hdr: NginxCacheHeader = unsafe { transmute(buf) };
                         if hdr.check_magic() {
                             let mut u = String::new();
-                            f.read_line(&mut u).ok().and_then(|_| Url::parse(&*u).ok())
-                                .map(|u| (Path::new(&p).to_owned(), u))
+                            f.read_line(&mut u).ok().and_then(|_| Url::parse(&*u).ok()).map(|u| (p, u))
                         } else {
                             None
                         }
